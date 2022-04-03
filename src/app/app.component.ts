5b483e5eea73c8f5e3005f69f23d8843aa9d1c49
TimsGitHubProjects <timgabrikowski@outlook.de>
2022-04-03 14:26:30 +0200

    password?!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MessageService } from './message.service';
 
 @Component({
 	selector: 'app-root',
@@ -6,6 +7,29 @@ import { Component } from '@angular/core';
 	styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-	title = 'TiBoLi-FE-Local';
+	title = 'TiBoLi';
 	footerText = 'Tim Gabrikowski';
+
+	constructor(private messageService: MessageService) {}
+
+	password = 'TiBoLi';
+
+	loggedIn: boolean = false;
+
+	OnInit() {
+		if (localStorage.getItem('loggedIn') == 'true') {
+			this.loggedIn = true;
+		} else {
+			this.loggedIn = false;
+		}
+	}
+
+	login(password: string) {
+		if (password == this.password) {
+			this.loggedIn = true;
+			localStorage.setItem('loggedIn', 'true');
+		} else {
+			this.messageService.add('FALSCH!');
+		}
+	}
 }
