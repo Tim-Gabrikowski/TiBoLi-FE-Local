dce5f43fafd682e9d2f41f9a6eab6cf22051b10f
TimsGitHubProjects <timgabrikowski@outlook.de>
2022-05-01 15:35:44 +0200

    (re)print labels for existing copies

--- src/app/books/book-detail/book-copies/book-copies.component.ts
+++ src/app/books/book-detail/book-copies/book-copies.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { IBook } from 'src/app/interfaces/book';
+import { ICopy } from 'src/app/interfaces/copy';
+import { PdfService } from 'src/app/pdf.service';
 import { BookService } from '../../services/book.service';
 
 @Component({
@@ -8,7 +10,10 @@ import { BookService } from '../../services/book.service';
 	styleUrls: ['./book-copies.component.css'],
 })
 export class BookCopiesComponent implements OnInit {
-	constructor(private bookService: BookService) {}
+	constructor(
+		private bookService: BookService,
+		private pdfService: PdfService
+	) {}
 
 	@Input() book?: IBook;
 	newCopy: boolean = false;
@@ -35,4 +40,139 @@ export class BookCopiesComponent implements OnInit {
 			(copy) => copy.lifecycle != 5
 		);
 	}
+
+	printCopies: ICopy[] = [];
+	loadLables: boolean = false;
+	addToPrint(mNumber: Number, copyID: Number, lifecycle: Number) {
+		var copy = {
+			mNumber: mNumber,
+			copyId: copyID,
+			lifecycle: lifecycle,
+			bookId: this.book!.id,
+		};
+		this.printCopies.push(copy);
+		console.log('print: ', copy);
+	}
+	removeFromPrint(mNumber: Number) {
+		this.printCopies = this.printCopies.filter((e) => e.mNumber != mNumber);
+	}
+	isCopyForPrint(mNumber: Number) {
+		if (this.printCopies.filter((e) => e.mNumber == mNumber).length > 0) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	getPdfOfBooks(startAt: string) {
+		var body = {
+			startAt: Number(startAt),
+			books: [{}],
+		};
+
+		var books = [{}];
+		this.printCopies.forEach((element) => {
+			var book = {
+				title: this.book!.title,
+				author: this.book!.author,
+				mNumber: element.mNumber,
+			};
+			books.push(book);
+		});
+
+		body.books = books;
+		body.books.shift();
+		console.log(body);
+		this.loadLables = true;
+		this.pdfService.downloadPDF(body, () => {
+			this.loadLables = false;
+			this.printCopies = [];
+		});
+	}
+	defaultLabelPosition = 0;
+	labelPositions = [
+		{
+			position: 0,
+			label: '1:1',
+		},
+		{
+			position: 1,
+			label: '1:2',
+		},
+		{
+			position: 2,
+			label: '1:3',
+		},
+		{
+			position: 3,
+			label: '2:1',
+		},
+		{
+			position: 4,
+			label: '2:2',
+		},
+		{
+			position: 5,
+			label: '2:3',
+		},
+		{
+			position: 6,
+			label: '3:1',
+		},
+		{
+			position: 7,
+			label: '3:2',
+		},
+		{
+			position: 8,
+			label: '3:3',
+		},
+		{
+			position: 9,
+			label: '4:1',
+		},
+		{
+			position: 10,
+			label: '4:2',
+		},
+		{
+			position: 11,
+			label: '4:3',
+		},
+		{
+			position: 12,
+			label: '5:1',
+		},
+		{
+			position: 13,
+			label: '5:2',
+		},
+		{
+			position: 14,
+			label: '5:3',
+		},
+		{
+			position: 15,
+			label: '6:1',
+		},
+		{
+			position: 16,
+			label: '6:2',
+		},
+		{
+			position: 17,
+			label: '6:3',
+		},
+		{
+			position: 18,
+			label: '7:1',
+		},
+		{
+			position: 19,
+			label: '7:2',
+		},
+		{
+			position: 20,
+			label: '7:3',
+		},
+	];
 }
