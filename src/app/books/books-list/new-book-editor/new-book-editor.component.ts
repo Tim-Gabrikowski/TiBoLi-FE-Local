e8e17a82dc4403db1f5379c218a57fa67aa125f5
TimsGitHubProjects <timgabrikowski@outlook.de>
2022-05-01 08:59:55 +0200

    Intergade print on new book create

--- src/app/books/books-list/new-book-editor/new-book-editor.component.ts
+++ src/app/books/books-list/new-book-editor/new-book-editor.component.ts
@@ -69,9 +69,9 @@ export class NewBookEditorComponent implements OnInit {
 	reloadBookList() {
 		this.router.navigate(['/books/' + this.newBookID]);
 	}
-	getPdfOfBooks() {
+	getPdfOfBooks(startAt: string) {
 		var body = {
-			startAt: 0,
+			startAt: Number(startAt),
 			books: [{}],
 		};
 
@@ -98,6 +98,86 @@ export class NewBookEditorComponent implements OnInit {
 		{
 			position: 0,
 			label: '1:1',
+		},
+		{
+			position: 1,
+			label: '1:2',
+		},
+		{
+			position: 2,
+			label: '1:3',
+		},
+		{
+			position: 3,
+			label: '2:1',
+		},
+		{
+			position: 4,
+			label: '2:2',
+		},
+		{
+			position: 5,
+			label: '2:3',
+		},
+		{
+			position: 6,
+			label: '3:1',
+		},
+		{
+			position: 7,
+			label: '3:2',
+		},
+		{
+			position: 8,
+			label: '3:3',
+		},
+		{
+			position: 9,
+			label: '4:1',
+		},
+		{
+			position: 10,
+			label: '4:2',
+		},
+		{
+			position: 11,
+			label: '4:3',
+		},
+		{
+			position: 12,
+			label: '5:1',
+		},
+		{
+			position: 13,
+			label: '5:2',
+		},
+		{
+			position: 14,
+			label: '5:3',
+		},
+		{
+			position: 15,
+			label: '6:1',
+		},
+		{
+			position: 16,
+			label: '6:2',
+		},
+		{
+			position: 17,
+			label: '6:3',
+		},
+		{
+			position: 18,
+			label: '7:1',
+		},
+		{
+			position: 19,
+			label: '7:2',
+		},
+		{
+			position: 20,
+			label: '7:3',
 		},
 	];
 }
