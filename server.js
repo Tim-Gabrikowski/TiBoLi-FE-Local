8890b70dbc71f979453fb9d6f5fcfd986c5921a1
TimsGitHubProjects <timgabrikowski@outlook.de>
2022-06-16 18:41:41 +0200

    logging?

--- server.js
+++ server.js
@@ -5,6 +5,18 @@ const colors = require("colors");
 const app = express();
 const port = process.env.BUILD_PORT || 8083;
 
+var fs = require("fs");
+var util = require("util");
+var logFile = fs.createWriteStream("log.txt", { flags: "a" });
+// Or 'w' to truncate the file every time the process starts.
+var logStdout = process.stdout;
+
+console.log = function () {
+	logFile.write(util.format.apply(null, arguments) + "\n");
+	logStdout.write(util.format.apply(null, arguments) + "\n");
+};
+console.error = console.log;
+
 const booksApiRouter = require(process.env.RELATIVE_PATH_TO_BACKEND_MODULE);
 
 app.use("/api/", booksApiRouter);
